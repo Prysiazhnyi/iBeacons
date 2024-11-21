ffd40ae4572f59f88c628c6b44fbd0e131517133
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-21 18:41:45 +0200

    Initial Commit

---
+++ Project-22-iBeacons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-22-iBeacons
+//
+//  Created by Serhii Prysiazhnyi on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
